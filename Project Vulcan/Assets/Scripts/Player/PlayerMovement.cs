83a983e406914ced62d8dc787a97c1d8bb73aee6
iGalich <89650689+iGalich@users.noreply.github.com>
2022-04-17 22:13:06 +0300

    Wall Jumping added

--- Project Vulcan/Assets/Scripts/Player/PlayerMovement.cs
+++ Project Vulcan/Assets/Scripts/Player/PlayerMovement.cs
@@ -6,21 +6,25 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float _speed = 10f;
     [SerializeField] private float _jumpPower = 20f;
     private float _horizontalInput;
+    private bool _isInControl;
 
     [Header ("Coyote Time")]
     [SerializeField] private float _coyoteTime = 0.25f;
     private float _coyoteCounter;
 
     [Header ("Wall Jump")]
-    [SerializeField] private float _wallSlideSpeed = 0.3f;
     [SerializeField] private float _wallDistance = 0.55f;
-    [SerializeField] private float _wallSlideTime = 1f;
-    private float _lastWallSlide = Mathf.Infinity;
+    [SerializeField] private float _wallJumpTime = 0.2f;
+    private float _wallJumpCounter = Mathf.NegativeInfinity;
+    private float _initialGravity;
+    private bool _canGrabWall;
+    private bool _isGrabbingWall;
     private RaycastHit2D _wallHit;
-    private bool _isWallSliding = false;
+    private BoxCollider2D _lastWall;
 
     [Header ("Layers")]
     [SerializeField] private LayerMask _groundLayer;
+    [SerializeField] private LayerMask _wallLayer;
 
     // References
     private BoxCollider2D _collider;
@@ -30,91 +34,100 @@ public class PlayerMovement : MonoBehaviour
     {
         _collider = GetComponent<BoxCollider2D>();
         _body = GetComponent<Rigidbody2D>();
+        _initialGravity = _body.gravityScale;
     }
 
     private void Update()
     {
         _horizontalInput = Input.GetAxis("Horizontal");
-        
-        FlipSprite(_horizontalInput);
+
+        _isInControl = CheckIfInControl();
+        if (!_isInControl)
+        {
+            _wallJumpCounter -= Time.deltaTime;
+        }
+
+        FlipSprite();
+
         if (Input.GetKeyDown(KeyCode.Space))
             Jump();
+
+        CheckForWall();
     }
 
     private void FixedUpdate() 
     {
         BasicMovement(_horizontalInput);
-        WallCheck();
     }
 
-    private void WallCheck()
+    private bool CheckIfInControl()
     {
-        _wallHit = Physics2D.Raycast(transform.position, new Vector2(_wallDistance * Mathf.Sign(transform.localScale.x), 0), _wallDistance, _groundLayer);
-        Debug.DrawRay(transform.position, new Vector2(_wallDistance, 0), Color.red); // TODO remove
+        return _wallJumpCounter <= 0;
+    }
 
-        if (!_wallHit)
+    private void CheckForWall()
+    {
+        _canGrabWall = IsOnWall();
+        _isGrabbingWall = false;
+
+        if (_canGrabWall && !IsGrounded())
         {
-            _isWallSliding = false;
+            // TODO check if this entire line is really necessary
+            _isGrabbingWall = (transform.localScale.x > 0 && _horizontalInput > 0) || (transform.localScale.x < 0 && _horizontalInput < 0);
         }
 
-        if (_wallHit && !IsGrounded() && !_isWallSliding)
+        if (_isGrabbingWall && _isInControl)
         {
-            _isWallSliding = true;
-            _lastWallSlide = Time.time;
-        }
-
-        if (Time.time - _lastWallSlide < _wallSlideTime && _isWallSliding)
-        {
-            _body.velocity = new Vector2(_body.velocity.x, Mathf.Clamp(_body.velocity.y, _wallSlideSpeed, float.MaxValue));
-        }
-
-        if (IsGrounded())
-        {
-            _isWallSliding = false; 
-            _coyoteCounter = _coyoteTime;
+            _body.gravityScale = 0;
+            _body.velocity = Vector2.zero;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _wallJumpCounter = _wallJumpTime;
+                _isInControl = false;
+                _body.gravityScale = _initialGravity;
+                _body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * _speed, _jumpPower);
+                _isGrabbingWall = false;
+            }
         }
         else
         {
-            _coyoteCounter -= Time.deltaTime;
+            _body.gravityScale = _initialGravity;
         }
-    }
-
-    private bool OnWall()
-    {
-        return _wallHit;
     }
 
     private void Jump()
     {
-        if (IsGrounded())
+        if (_isInControl && IsGrounded())
+        {
             _body.velocity = new Vector2(_body.velocity.x, _jumpPower);
-        //else if (OnWall())
-        //    WallJump();
-        else if (_coyoteCounter > 0 || OnWall())
-            _body.velocity = new Vector2(_body.velocity.x, _jumpPower);
-    }
-
-    private void WallJump()
-    {
-        _body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x), _jumpPower);
+        }
     }
     
     private void BasicMovement(float input)
     {
-        _body.velocity = new Vector2(_horizontalInput * _speed, _body.velocity.y);
+        if (_isInControl)
+            _body.velocity = new Vector2(_horizontalInput * _speed, _body.velocity.y);
     }
 
-    private void FlipSprite(float input)
+    private void FlipSprite()
     {
-        if (input > 0.01f)
+        if (_body.velocity.x > 0.01f)
             transform.localScale = Vector3.one;
-        else if (input < -0.01f)
+        else if (_body.velocity.x < -0.01f)
             transform.localScale = new Vector3(-1, 1, 1);
+    }
+
+    private bool IsOnWall()
+    {
+        _wallHit = Physics2D.Raycast(transform.position, new Vector2(_wallDistance * Mathf.Sign(transform.localScale.x), 0), _wallDistance, _wallLayer);
+        Debug.DrawRay(transform.position, new Vector2(_wallDistance, 0) * Mathf.Sign(transform.localScale.x), Color.red);
+        return _wallHit;
     }
 
     private bool IsGrounded()
     {
         RaycastHit2D raycastHit = Physics2D.BoxCast(_collider.bounds.center, _collider.bounds.size, 0 , Vector2.down, 0.1f, _groundLayer);
+        
         return raycastHit.collider != null;
     }
 }
